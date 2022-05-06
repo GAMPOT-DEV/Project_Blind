f7e65598623ce63fbc83684429593361ccbbc720
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-05-06 11:30:57 +0900

    feature : 상태머신 추가

---
+++ Project_Blind/Assets/Scripts/Core/StateMachineBehaviour/Player/JumpingSMB.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Animations;
+namespace Blind
+{
+    /// <summary>
+    /// 공중에 떠있을 때에 상태를 정의합니다.
+    /// </summary>
+    public class JumpingSMB : SceneLinkedSMB<PlayerCharacter>
+    {
+        public override void OnSLStateNoTransitionUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex,
+            AnimatorControllerPlayable controller)
+        {
+            _monoBehaviour.Jump();
+            _monoBehaviour.UpdateJump();
+            _monoBehaviour.AirborneVerticalMovement();
+            _monoBehaviour.GroundedHorizontalMovement(true);
+            _monoBehaviour.CheckForGrounded();
+            _monoBehaviour.UpdateVelocity();
+        }
+    }
+}
