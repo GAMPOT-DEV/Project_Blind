1657ec8b7d1a2e3da6eaaf5581a57f56a41cd2b8
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-05-20 21:13:06 +0900

    feature : 파동 로직 구현

---
+++ Project_Blind/Assets/Scripts/Core/MonoBehaviour/WaveHitObj.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Blind
+{
+    /// <summary>
+    /// 파동에 맞은 오브젝트를 처리하는 클래스입니다.
+    /// </summary>
+    public class WaveHitObj : MonoBehaviour
+    {
+        public void Awake()
+        {
+            GetComponent<Light2D>().intensity = 0;
+        }
+
+        public void GetHit()
+        {
+            StartCoroutine(Glow());
+        }
+
+        private IEnumerator Glow()
+        {
+            GetComponent<Light2D>().intensity = 1;
+            yield return new WaitForSeconds(5f);
+            GetComponent<Light2D>().intensity = 0;
+        }
+    }
+}
