3d1c09468de3b9540a21d9f83ccb9ed388d4235f
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-05-24 14:38:15 +0900

    feature : 유닛 체력 관리 코드 추가

---
+++ Project_Blind/Assets/Scripts/Core/MonoBehaviour/UnitHP.cs
@@ -0,0 +1,24 @@
+namespace Blind
+{
+    /// <summary>
+    /// 유닛의 체력을 관리하는 클래스입니다. 
+    /// </summary>
+    public class UnitHP : IDamageable<float>
+    {
+        private float _health;
+        private float _maxHealth;
+        public UnitHP(int maxHealth) {
+            _health = _maxHealth;
+            _maxHealth = maxHealth;
+        }
+        public void GetDamage(float damage) {
+            _health -= damage;
+        }
+        public void GetHeal(float heal) {
+            _health += heal;
+        }
+        public float GetHP() {
+            return _health;
+        }
+    }
+}
