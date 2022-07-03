47847bcdc9749263537c2877d185b14c17e5ed31
kimmoonwoong <ansdnd1500@naver.com>
2022-07-03 15:03:50 +0900

    feature: 싱글톤 상속으로 교체

--- Project_Blind/Assets/Scripts/Utils/PhysicHelper.cs
+++ Project_Blind/Assets/Scripts/Utils/PhysicHelper.cs
@@ -7,37 +7,8 @@ namespace Blind
     /// <summary>
     /// 현재는 레이캐스트가 맞은 오브젝트에 PlatformEffector가 있는지 판별하는 클래스(나중에 플랫폼 이펙트 말고도 다른 걸 판별할 때 써도됨)
     /// </summary>
-    public class PhysicHelper : MonoBehaviour
+    public class PhysicHelper : Singleton<PhysicHelper>
     {
-        private static PhysicHelper s_Instance;
-
-        static PhysicHelper Instance
-        {
-            get
-            {
-                if (s_Instance != null)
-                    return s_Instance;
-
-                s_Instance = FindObjectOfType<PhysicHelper> ();
-
-                if (s_Instance != null)
-                    return s_Instance;
-            
-                Create ();
-            
-                return s_Instance;
-            }
-            set
-            {
-                s_Instance = value;
-            }
-        }
-
-        static void Create()
-        {
-            GameObject physicsHelperGameObject = new GameObject("PhysicsHelper");
-            s_Instance = physicsHelperGameObject.AddComponent<PhysicHelper> ();
-        }
         Dictionary<Collider2D, PlatformEffector2D> m_PlatformEffectorCache = new Dictionary<Collider2D, PlatformEffector2D> ();
 
         private void Awake()
