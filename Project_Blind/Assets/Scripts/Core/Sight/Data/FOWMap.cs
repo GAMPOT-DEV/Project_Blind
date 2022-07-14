555917caffef225faa840777dbe905302b4ebb03
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-07-14 18:11:47 +0900

    feature : 타일맵 구성 세팅

---
+++ Project_Blind/Assets/Scripts/Core/Sight/Data/FOWMap.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Blind
+{
+    public class FOWMap
+    {
+        private List<FOWTile> map = new List<FOWTile>();
+    }
+}
--- Project_Blind/Assets/Scripts/Core/Sight/SightController.cs
+++ Project_Blind/Assets/Scripts/Core/Sight/SightController.cs
@@ -11,43 +11,81 @@ namespace Blind
         public int resolution = 512;
         [Range(0.01f, 1f)]
         public float brushSize = 0.1f;
-        public Texture2D brushTexture;
-        public GameObject target;
-        private Texture2D mainTex;
-        private MeshRenderer mr;
-        private RenderTexture rt;
+        public bool ShowGizmos = true;
+        
+        public const float CurrentAlpha = 0f; // 현재 위치한 경우 알파값
+        public const float NeverAlpha = 1f; // 방문한 적 없는 경우 알파값
+
+        public LayerMask _groundLayer;
+
+        [Space(8f)]
+        public float FogWidthX = 20;
+        public float FogWidthZ = 20;
+        public float TileSize = 1; // 타일 하나의 크기
+
+        
+        private Texture2D _brushTexture;
+        private Texture2D _mainTex;
+        private MeshRenderer _mr;
+        private RenderTexture _rt;
+        private RenderTexture _originalRT;
 
         protected override void Awake()
         {
             base.Awake();
-            TryGetComponent(out mr);
-            rt = new RenderTexture(resolution, resolution, 32);
+            GetComponent<MeshRenderer>().enabled = true;
+            TryGetComponent(out _mr);
+            _rt = new RenderTexture(resolution, resolution, 32);
 
-            if (mr.material.mainTexture != null)
+            if (_mr.material.mainTexture != null)
             {
-                mainTex = mr.material.mainTexture as Texture2D;
+                _mainTex = _mr.material.mainTexture as Texture2D;
             }
             // 메인 텍스쳐가 없을 경우, 하얀 텍스쳐를 생성하여 사용
             else
             {
-                mainTex = new Texture2D(resolution, resolution);
+                _mainTex = new Texture2D(resolution, resolution);
             }
 
             // 메인 텍스쳐 -> 렌더 텍스쳐 복제
-            Graphics.Blit(mainTex, rt);
+            Graphics.Blit(_mainTex, _rt);
 
             // 렌더 텍스쳐를 메인 텍스쳐에 등록
-            mr.material.mainTexture = rt;
+            _mr.material.mainTexture = _rt;
 
-            // 브러시 텍스쳐가 없을 경우 임시 생성(red 색상)
-            if (brushTexture == null)
+            // 시야 텍스쳐 추가
+            _brushTexture = new Texture2D(resolution, resolution);
+            int rSquared = (resolution/2) * (resolution/2);
+            int x = resolution / 2, y = x;
+            for (int u = 0; u < resolution; u++)
             {
-                brushTexture = new Texture2D(resolution, resolution);
-                for (int i = 0; i < resolution; i++)
-                    for (int j = 0; j < resolution; j++)
-                        brushTexture.SetPixel(i, j, new Color(0,0,0,0.5f));
-                brushTexture.Apply();
+                for (int v = 0; v < resolution; v++)
+                {
+                    if ((x-u)*(x-u) + (y-v)*(y-v) < rSquared) {
+                        _brushTexture.SetPixel(u, v, new Color(0,0,0,1f));
+                    }
+                    else
+                    {
+                        _brushTexture.SetPixel(u, v, new Color(0,0,0,0));
+                    }
+                }
             }
+            _brushTexture.Apply();
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (Application.isPlaying == false) return;
+#if DEBUG_RANGE
+            foreach (var tile in visibleTiles)
+            {
+                Vector2 pos = GetTileCenterPoint(tile.x, tile.y);
+                Gizmos.color = Color.green;
+                Gizmos.DrawCube(new Vector3(pos.x, 0f, pos.y), new Vector3(_tileSize, 1f, _tileSize));
+            }
+#endif
+            if (ShowGizmos == false) return;
+            
         }
 
         public void DrawTexture(RaycastHit hit)
@@ -57,7 +95,6 @@ namespace Blind
             {
                 Vector2 pixelUV = hit.textureCoord;
                 pixelUV *= resolution;
-                Debug.Log(pixelUV);
                 _DrawTexture(pixelUV);
             }
         }
@@ -66,8 +103,8 @@ namespace Blind
         /// <summary> 렌더 텍스쳐에 브러시 텍스쳐로 그리기 </summary>
         private void _DrawTexture(in Vector2 uv)
         {
-            Debug.Log(uv);
-            RenderTexture.active = rt; // 페인팅을 위해 활성 렌더 텍스쳐 임시 할당
+            Graphics.Blit(_mainTex, _rt);
+            RenderTexture.active = _rt; // 페인팅을 위해 활성 렌더 텍스쳐 임시 할당
             GL.PushMatrix();                                  // 매트릭스 백업
             GL.LoadPixelMatrix(0, resolution, resolution, 0); // 알맞은 크기로 픽셀 매트릭스 설정
 
@@ -77,13 +114,13 @@ namespace Blind
             Graphics.DrawTexture(
                 new Rect(
                     uv.x - brushPixelSize * 0.5f,
-                    (rt.height - uv.y) - brushPixelSize * 0.5f,
+                    (_rt.height - uv.y) - brushPixelSize * 0.5f,
                     brushPixelSize,
                     brushPixelSize
                 ),
-                brushTexture
+                _brushTexture
             );
-
+            
             GL.PopMatrix();              // 매트릭스 복구
             RenderTexture.active = null; // 활성 렌더 텍스쳐 해제
         }
