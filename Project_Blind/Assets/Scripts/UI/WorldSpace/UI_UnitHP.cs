abb8320c487e1b76d9886795aa7d085c82ad882e
kjhcocomi <981028_@naver.com>
2022-08-03 13:42:57 +0900

    적 유닛과 플레이어 사이에 지형이 있으면 Hp UI 보이지 않도록 수정

--- Project_Blind/Assets/Scripts/UI/WorldSpace/UI_UnitHP.cs
+++ Project_Blind/Assets/Scripts/UI/WorldSpace/UI_UnitHP.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,6 +9,13 @@ namespace Blind
     {
         private UnitHP _unitHP;
         private CircleCollider2D _colider;
+        private bool _checkPlayer = false;
+        private int _mask;
+        private PlayerCharacter _player;
+
+        // 플레이어가 몬스터를 볼 수 있는 범위
+        private float _dist = 10.0f;
+
         public UnitHP HP
         {
             get { return _unitHP; }
@@ -30,6 +37,9 @@ namespace Blind
             base.Start();
             InitEvents();
             gameObject.GetComponent<Canvas>().enabled = false;
+
+            // 레이어 설정
+            _mask = LayerMask.GetMask("Player") | LayerMask.GetMask("Floor");
         }
         private void InitEvents()
         {
@@ -40,7 +50,7 @@ namespace Blind
             _colider = Util.GetOrAddComponent<CircleCollider2D>(gameObject);
             _colider.isTrigger = true;
             _colider.offset = new Vector3(0, -60f, 0);
-            _colider.radius = 200.0f;
+            _colider.radius = 400.0f;
         }
         private void OnHpChanged(float currHP, float maxHP)
         {
@@ -60,14 +70,56 @@ namespace Blind
         {
             if (collision.gameObject.GetComponent<PlayerCharacter>() == null)
                 return;
-            gameObject.GetComponent<Canvas>().enabled = true;
+
+            // 범위 안에 플레이어가 들어오면 레이캐스트를 써서 몬스터와 캐릭터 중간에 뭐가 있는지 확인
+            _checkPlayer = true;
+            _player = collision.gameObject.GetComponent<PlayerCharacter>();
+
+            StartCoroutine(CoCheckPlayer());
         }
 
         private void OnTriggerExit2D(Collider2D collision)
         {
             if (collision.gameObject.GetComponent<PlayerCharacter>() == null)
                 return;
-            gameObject.GetComponent<Canvas>().enabled = false;
+
+            // 플레이어가 범위에서 벗어나면 탐색 종료
+            _checkPlayer = false;
+            _player = null;
+        }
+        IEnumerator CoCheckPlayer()
+        {
+            while (true)
+            {
+                if (!_checkPlayer)
+                    break;
+                Vector3 myPos = Owner.transform.position;
+                Vector3 playerPos = _player.transform.position;
+                Vector3 dir = (playerPos - myPos).normalized;
+                // 내 위치에서 플레이어 위치로 레이캐스트
+                var result = Physics2D.Raycast(myPos, dir, _dist, _mask);
+                Debug.DrawRay(myPos, dir * _dist, Color.red, 0.1f);
+
+                //레이캐스트에 충돌이 됐을 때
+                if (result.collider != null)
+                {
+                    // 처음으로 충돌한게 플레이어라면
+                    if (result.collider.gameObject.GetComponent<PlayerCharacter>() != null)
+                    {
+                        // UI 보이게함
+                        gameObject.GetComponent<Canvas>().enabled = true;
+                    }
+                    else
+                    {
+                        gameObject.GetComponent<Canvas>().enabled = false;
+                    }
+                }
+                else
+                {
+                    gameObject.GetComponent<Canvas>().enabled = false;
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
         }
     }
 }
