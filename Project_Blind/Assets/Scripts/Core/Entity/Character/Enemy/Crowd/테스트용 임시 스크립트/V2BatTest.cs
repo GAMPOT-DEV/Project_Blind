3976b3ca8fe5cdfe7f9b92583e2e9d4e8cb99c9c
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-09-28 21:44:09 +0900

    feature : AI 좀 나아졌을지도

---
+++ Project_Blind/Assets/Scripts/Core/Entity/Character/Enemy/Crowd/테스트용 임시 스크립트/V2BatTest.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Blind
+{
+    public class V2BatTest : V2CrowdTest
+    {
+        [SerializeField] private Transform AttackHitBoxRange;
+
+        protected void Awake()
+        {
+            base.Awake();
+
+            Data.sensingRange = new Vector2(12f, 8f);
+            Data.speed = 0.1f ;
+            Data.runSpeed = 0.07f;
+            Data.attackCoolTime = 0.5f;
+            Data.attackSpeed = 0.3f;
+            Data.attackRange = new Vector2(9f, 8f);
+            Data.stunTime = 1.5f;
+            _patrolTime = 3f;
+        }
+
+        private void Start()
+        {
+            startingPosition = gameObject.transform;
+            _attack.Init(7, 10);
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+        }
+
+        protected override void updateAttack()
+        {
+            if (currentAttack == 0)
+            {
+                if (Random.Range(0, 100) > 20)
+                    currentAttack = 1;
+                else
+                    currentAttack = 2;
+                    
+            }
+
+            if(currentAttack == 1)
+                _anim.SetInteger("State", 30);
+            else if (currentAttack == 2)
+                _anim.SetInteger("State", 31);
+        }
+
+        public void AttackHitBox()
+        {
+            col = gameObject.AddComponent<BoxCollider2D>();
+            col.offset = new Vector2(_col.offset.x +3.5f, _col.offset.y);
+            col.size = new Vector2(7, 9);
+        }
+
+        public void DeadSound()
+        {
+            SoundManager.Instance.Play("Crowd/Bat/Death");
+        }
+    }
+}
