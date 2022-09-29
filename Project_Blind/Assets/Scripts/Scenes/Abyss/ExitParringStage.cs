392e7d0c62cd8fed3b0e2be133855e4e0260ffcd
hhtboy <hhtboy1024@gmail.com>
2022-09-29 22:50:03 +0900

    스테이지 관련 스크립트 수정

--- Project_Blind/Assets/Scripts/Scenes/Abyss/ExitParringStage.cs
+++ Project_Blind/Assets/Scripts/Scenes/Abyss/ExitParringStage.cs
@@ -7,9 +7,29 @@ namespace Blind.Abyss
     public class ExitParringStage : ExitPointStage
     {
         [SerializeField] private Wall _leaveWall;
-        private void Awake()
+        private PlayerCharacter player;
+        private bool isClear = false;
+        public GameObject monster;
+        protected override void Awake()
         {
+            _leaveWall.Enable();
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCharacter>();
             base.Awake();
+
+        }
+
+        private void Update()
+        {
+            if(player.isParingCheck)
+            {
+                _leaveWall.Disable();
+                isClear = true;
+                Debug.Log("wall disabled");
+            }
+            if(!monster.activeSelf && !isClear)
+            {
+                monster.GetComponent<CrowdEnemyCharacter>().Reset();
+            }
         }
     }
 }
